aef2f2c9381b0382158f748c998689b23d2b47df
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-01-02 14:05:59 +0700

    chapter_2

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,29 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get("http://localhost:8000")
 
-assert "Congratulations!" in browser.title
-print("Test OK!")
+class NewVisitorTest(unittest.TestCase):  
+    def setUp(self):  
+        self.browser = webdriver.Chrome()  
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):  
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")  
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)  
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")  
+
+        [...]
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == "__main__":  
+    unittest.main()  
