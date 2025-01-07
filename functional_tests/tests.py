4189e50f620dbc3ce17cf3eacce950d1b0a42395
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-01-08 00:26:58 +0700

    end_chapter_6

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,10 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
-import unittest
 
-
+MAX_WAIT = 5 
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -33,14 +33,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")  
@@ -72,3 +72,16 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
         self.browser.get(self.live_server_url)
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element(By.ID, "id_nothing")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise  
+                time.sleep(0.5)  
