2fa702cd18af2a8d651a4d20e615db58f626dac3
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-01-08 14:31:00 +0700

    new URL + view for adding to existing lists. FT passes :-)

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.home_page, name="home"), 
     path('admin/', admin.site.urls),
     path("lists/new", views.new_list, name="new_list"),
-    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"), 
+    path("lists/<int:list_id>/", views.view_list, name="view_list"),
+    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
+
 ]
