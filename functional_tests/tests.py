2c3a2b0e20974c9c5530348f76ede8c955f2b321
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-01-08 13:35:17 +0700

    end_chapter6_fix

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -42,35 +42,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
         self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")  
-        self.assertIn(
-            "1: Buy peacock feathers",
-            [row.text for row in rows],
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows],
-        )
-
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
-
-        # There is still a text box inviting her to add another item.
-        # She enters "Use peacock feathers to make a fly"
-        # (Edith is very methodical)
-        # The page updates again, and now shows both items on her list
-
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
-    def test_can_start_a_todo_list(self):
-        # Edith has heard about a cool new online to-do app.
-        # She goes to check out its homepage
         self.browser.get(self.live_server_url)
 
     def wait_for_row_in_list_table(self, row_text):
@@ -85,3 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:  
                     raise  
                 time.sleep(0.5)  
+    
+    
+
+    
